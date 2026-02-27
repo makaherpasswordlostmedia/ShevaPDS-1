64c95212c5bb0ebfbdf4c19b59708a473ed26fc5
whyineedname <emssaoig1@gmail.com>
2026-02-27 10:06:14 +0200

    Add files via upload

--- app/src/main/java/com/imlac/pds1/EmulatorActivity.java
+++ app/src/main/java/com/imlac/pds1/EmulatorActivity.java
@@ -84,8 +84,12 @@ public class EmulatorActivity extends Activity {
 
     private void hideSystemUI() {
         getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            View.SYSTEM_UI_FLAG_FULLSCREEN         |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION    |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY   |
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE      |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN  |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
     }
 
     private void findViews() {
